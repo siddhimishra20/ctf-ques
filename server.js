9fc371a39cfae2b11c26765c70419722c45ba6a1
Siddhi Mishra <f20230020@dubai.bits-pilani.ac.in>
2024-11-26 22:50:24 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware to parse the URL-encoded body
 app.use(express.urlencoded({ extended: true }));
 
-// Main route - Clue page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Route for clue submission
 app.post('/check-clue', (req, res) => {
     const clueInput = req.body.clue.toLowerCase().trim();
     const correctClue = "it lies where knowledge flows";
@@ -33,11 +29,9 @@ app.post('/check-clue', (req, res) => {
     }
 });
 
-
 app.get('/knowledge', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'knowledge', 'index.html'));
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
